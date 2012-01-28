bd2e74a24658aa897baa8e36614803f99555ce83
Helios Vicente <helios.vicente@gmail.com>
2012-01-28 13:00:17 +0100

    Listing 9.17 authenticate_with_salt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,13 @@ class SessionsController < ApplicationController
                             params[:session][:password])
     if user.nil?
       # Create an error message and re-render the signin form
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
     else
       # Sign the user in and redirect to the user's show page
+      sign_in user
+      redirect_to user
     end
   end
 

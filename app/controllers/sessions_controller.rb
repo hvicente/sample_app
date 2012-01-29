33ca05a71fb135d1536170702c888b7fe7c17d91
Helios Vicente <helios.vicente@gmail.com>
2012-01-29 10:15:51 +0100

    Chapter 9 finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.authenticate(params[:session][:email],
-                            params[:session][:password])
+                             params[:session][:password])
     if user.nil?
       # Create an error message and re-render the signin form
       flash.now[:error] = "Invalid email/password combination."
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    sign_out
+    redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,11 +2,20 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    self.current_user = user
+    current_user = user
   end
 
   def current_user
     @current_user ||= user_from_remember_token
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    current_user = nil
   end
 
   private
